443e50e4e3fcf333a4b9f5b3da0c688e989dfe96
Russkikh Konstanin <russ.kostua83@gmail.com>
2023-05-04 23:26:46 +0300

    Initial commit

---
+++ test_google_finds_selene.py
@@ -0,0 +1,12 @@
+from selene.support.shared import browser
+from selene import have, be
+
+browser.open("https://google.com")
+
+browser.element('[name=q]')\
+    .should(be.blank)\
+    .send_keys('selene')\
+    .press_enter()
+browser.element('[id=search]')\
+    .should(have.text('yashaka/selene: User-oriented Web UI browser tests in ...'))
+
